a2636f7ac2af681a585a634c02d51433adb027bb
Ed. Gi <31946346+dwaxgio@users.noreply.github.com>
2020-09-24 12:36:44 -0500

    Controlador, ViewModel y Vista de cargue de archivos

---
+++ Controllers/ArchivoController.cs
@@ -0,0 +1,45 @@
+﻿using CursoMVC.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CursoMVC.Controllers
+{
+    // 21. Creo controlador (luego de crear carpeta Files) para gestionarlos, no es necesario el uso de db
+    public class ArchivoController : Controller
+    {
+        // GET: Archivo
+        public ActionResult Index()
+        {
+            if (TempData["Message"] != null)
+            {
+                ViewBag.Message = TempData["Message"].ToString(); // ViewBag sólo vive en la vista que se está invocando
+            }
+            return View();
+        }
+
+        // 24. Creo método que recibirá la información del formulario
+        [HttpPost]
+        public ActionResult Save(ArchivoViewModel model)
+        {
+            string RutaSitio = Server.MapPath("~/");
+            string PathArchivo1 = Path.Combine(RutaSitio + "/Files/archivo1.png");
+            string PathArchivo2 = Path.Combine(RutaSitio + "/Files/archivo2.png");
+
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model); // Si la información enviada por el modelo no es valida, entonces redirija a Index, con la información que se haya llenado de model
+            }
+
+            // Para guardar en carpeta Files
+            model.Archivo1.SaveAs(PathArchivo1);
+            model.Archivo2.SaveAs(PathArchivo2);
+
+            @TempData["Message"] = "Se cargaron los archivos";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/ViewModels/ArchivoViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CursoMVC.Models.ViewModels
+{
+    // 22. Creo ViewModel para el cargue de los archivos
+    public class ArchivoViewModel
+    {
+        // Estos ´harian relacion a los "campos" que se van a remitir desde un formulario
+        [Required]
+        [DisplayName("Mi archivo")] // es lo que aparecerá en el label
+        public HttpPostedFileBase Archivo1 { get; set; }
+        [Required]
+        [DisplayName("Mi archivo2")]
+        public HttpPostedFileBase Archivo2 { get; set; }
+        [Required]
+        [DisplayName("Mi cadena")]
+        public string Cadena { get; set; }
+    }
+}
