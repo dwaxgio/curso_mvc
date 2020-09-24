b7e8a3a0d708c4e7b769f381c1e5c7132beec485
Ed. Gi <31946346+dwaxgio@users.noreply.github.com>
2020-09-23 20:14:15 -0500

    Se agrega metodo Enter en controllers, para gestionar el login

--- Controllers/AccessController.cs
+++ Controllers/AccessController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CursoMVC.Models; // Importo "carpeta" para poder acceder a los modelos
 
 namespace CursoMVC.Controllers
 {
@@ -20,7 +21,27 @@ namespace CursoMVC.Controllers
         {
             try
             {
-                return Content("1");
+                // 5. Luego de agregar EF en Models, procedo a realizar la siguiente validación
+                using (CursomvcEntities db = new CursomvcEntities()) // Creo mi objeto EF
+                {
+                    var lst = from d in db.user // d es un alias, sería como el SELECT, el in seria como el FROM
+                              where d.email == user && d.password == password && d.idState == 1
+                              select d; // select d trae todo lo de la tabla users, podria definir un campo en especifico asi: select d.password
+
+                              if(lst.Count() > 0) // valida si se encuentra el registro en la db
+                              {
+                                    user oUser = lst.First();
+                                    Session["User"] = oUser; // Session["User"] es como se maneja dato de sesión en C#
+                                    return Content("1");
+                    }
+                              else
+                              {
+                                 return Content("Usuario inválido");
+                              }
+                }
+
+
+                
             }
             catch (Exception ex)
             {
