019c40e458ca2dd70737da8237e2e9d26c64a841
Ed. Gi <31946346+dwaxgio@users.noreply.github.com>
2020-09-24 00:53:04 -0500

    Create de Usuarios, así como generación del formulario en vista

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using CursoMVC.Models;
 using CursoMVC.Models.TableViewModels;
+using CursoMVC.Models.ViewModels;
 
 namespace CursoMVC.Controllers
 {
@@ -33,6 +34,40 @@ namespace CursoMVC.Controllers
             }
 
             return View(lst);
+        }
+
+        // 15. Se agrega nuevo metodo, y otro igual, que recibe parametros
+        [HttpGet] 
+        public ActionResult Add() // En este método, se remite a la vista de formulario desde el que se ENVÍA la información 
+        {
+            return View();
+        }
+
+        [HttpPost] // Metodo para que información solo entre por post
+        public ActionResult Add(UserViewModel model) // En este método, se RECIBE la información desde el formulario
+        {
+            // 16. se valida la información recibida y se guarda en la DB
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            
+            //guardado en DB
+            using (var db = new CursomvcEntities())
+            {
+                user oUser = new user(); // Como voy a registrar un nuevo usuario, instancio el objeto, y empiezo a asignarle sus valores, acorde al modelo
+                oUser.idState = 1;
+                oUser.email = model.Email;
+                oUser.edad = model.Edad;
+                oUser.password = model.Password;
+
+                db.user.Add(oUser);
+
+                db.SaveChanges(); // Importante incluir metodo SaveChanges, para que quede almacenado el registro
+            }
+
+            // Una vez almacenado el registro, se re dirige a la vista del listado de usuarios, mediante el metodo Index() de este controlador
+            return Redirect(Url.Content("~/User/"));
         }
     }
 }
---
+++ Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; // para usar anotaciones []
+
+namespace CursoMVC.Models.ViewModels
+{
+    // 14. se agrega ViewModel para User para crud (para llenar el formulario)
+    public class UserViewModel
+    {
+        [Required]
+        [EmailAddress] //Valida que sea correo
+        [Display(Name = "Correo Electrónico")]
+        [StringLength(100,ErrorMessage ="El {0} debe tener al menos {1} caracteres", MinimumLength = 1)]
+        public string Email{ get; set; }
+
+        [Required]
+        [DataType(DataType.Password)] // Ya se inclye como contraseña en la petición
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirma contraseña")]
+        [DataType(DataType.Password)] // Ya se inclye como contraseña en la petición
+        [Compare("Password",ErrorMessage ="Las contraseñas no son iguales")]
+        public string ConfirmPassword{ get; set; }
+
+        [Required]
+        public int Edad { get; set; }
+    }
+}
